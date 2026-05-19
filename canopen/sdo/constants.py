d087e2c4814d27fb23efab42060670a127921029
Davy Gillebert <d.gillebert@aemics.nl>
2026-05-19 16:08:35 +0200

    Fixed some review comments and extended tests to cover more use cases

--- canopen/sdo/constants.py
+++ canopen/sdo/constants.py
@@ -2,7 +2,7 @@ import struct
 
 # Command, index, subindex
 SDO_STRUCT = struct.Struct("<BHB")
-SDO_BLOCKINIT_STRUCT = "<BHBI"  # Command + seqno, index, subindex, size
+SDO_BLOCKINIT_STRUCT = struct.Struct("<BHBI")  # Command + seqno, index, subindex, size
 SDO_BLOCKACK_STRUCT = struct.Struct("<BBB") # c + ackseq + new blocksize
 SDO_BLOCKEND_STRUCT = struct.Struct("<BH") # c + CRC
 SDO_ABORT_STRUCT = struct.Struct("<BHBI") # c +i + si + Abort code
